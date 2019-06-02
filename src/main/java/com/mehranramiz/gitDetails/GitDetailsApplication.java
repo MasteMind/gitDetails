9ac8342a6a73620cf5e1dde153ef12c3e76d7c51
ramiz.mehran <ramiz.mehran@payu.in>
2019-06-02 17:15:19 +0530

    base project

---
+++ src/main/java/com/mehranramiz/gitDetails/GitDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.mehranramiz.gitDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehranramiz/gitDetails/GitDetailsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mehranramiz.gitDetails;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitDetailsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
